c2c6c40abe4d52125b06cbc81ab68411ca3788fc
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2021-09-09 00:14:39 +0800

    keep configuration handling out of the website class

--- src/cdc_booker/cdc_booker.py
+++ src/cdc_booker/cdc_booker.py
@@ -6,6 +6,7 @@ import click
 import yaml
 
 from cdc_website import CDCWebsite, Types
+from cdc_notifier import CDCNotifier
 
 
 @click.command()
@@ -22,12 +23,20 @@ def main(username, password_, configuration, telegram):
     if configuration is not None:
         with open(configuration, "r") as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
+
+    username = config.get("username", username)
+    password = config.get("password", password_)
+    telegram = config.get("telegram", telegram)
+    refresh_rate = config.get("refresh_rate", 60)
+    notifier = CDCNotifier(
+        token=str(config.get("telegram_token", "")),
+        chat_id=str(config.get("telegram_chat_id", "")),
+    )
+
     with CDCWebsite(
         username=username,
-        password=password_,
+        password=password,
         headless=False,
-        telegram=telegram,
-        configuration=config,
     ) as cdc_website:
         cdc_website.open_home_website()
         cdc_website.login()
@@ -36,15 +45,23 @@ def main(username, password_, configuration, telegram):
             cdc_website.open_practical_lessons_booking(type=Types.PRACTICAL)
 
             try:
-                print(cdc_website.get_session_available_count())
+                session_count = cdc_website.get_session_available_count()
                 available_sessions = cdc_website.get_available_sessions()
+                print(f"Available slots: {session_count}")
                 print(f"available sessions: {pprint.pprint(available_sessions)}")
+
+                if telegram:
+                    notifier.send_message(f"Available slots: {session_count}")
+                    notifier.send_message(
+                        f"Available sessions: {pprint.pprint(available_sessions)}"
+                    )
+
             except Exception:
                 traceback.print_exc()
 
             # keep in the page
-            print("Sleeping for 60s...")
-            time.sleep(60)
+            print(f"Sleeping for {refresh_rate}s...")
+            time.sleep(refresh_rate)
 
 
 if __name__ == "__main__":
