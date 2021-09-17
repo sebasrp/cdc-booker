bbcb1fdb2418ad0c054782e24509618fc484135c
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2021-09-17 10:45:08 +0800

    try captcha solver for rotated letters

--- src/cdc_booker/captcha.py
+++ src/cdc_booker/captcha.py
@@ -97,11 +97,68 @@ def resolve_4(path):
     print("detected: " + detected)
 
 
+def resolve_5(path):
+    """https://stackoverflow.com/questions/61224108/tesseract-ocr-not-recognizing-multiple-characters-in-a-single-image"""
+    img = cv2.imread(path)
+    cv2.imshow("original", img)
+
+    # turn into gray for next processing
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV * cv2.THRESH_OTSU)[1]
+    thresh = cv2.bitwise_not(thresh)
+
+    # dilate to make the line thicker
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 12))
+    dilation = cv2.dilate(thresh, kernel, iterations=1)
+
+    # find the contour
+    cntrs = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cntrs = cntrs[0] if len(cntrs) == 2 else cntrs[1]
+
+    result = img.copy()
+    for c in cntrs:
+        # for each letter, create red rectangle
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(result, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # prepare letter for OCR
+        box = thresh[y : y + h - 2, x : x + w]
+        box = cv2.bitwise_not(box)
+        box = cv2.GaussianBlur(box, (3, 3), 0)
+
+        # retreive the angle. For the meaning of angle, see below
+        # https://namkeenman.wordpress.com/2015/12/18/open-cv-determine-angle-of-rotatedrect-minarearect/
+        rect = cv2.minAreaRect(c)
+        angle = rect[2]
+
+        # put angle below letter
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        bottomLeftCornerOfText = (x, y + h + 20)
+        fontScale = 0.6
+        fontColor = (255, 0, 0)
+        lineType = 2
+        cv2.putText(
+            result,
+            str(angle),
+            bottomLeftCornerOfText,
+            font,
+            fontScale,
+            fontColor,
+            lineType,
+        )
+
+        # do the OCR
+        custom_config = r"-l eng --oem 3 --psm 10"
+        text = pytesseract.image_to_string(box, config=custom_config)
+        print("Detected :" + text + ", angle: " + str(angle))
+
+
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("path", help="Captcha file path")
     args = argparser.parse_args()
     path = args.path
     print("Resolving Captcha")
-    captcha_text = resolve_3_1(path)
+    captcha_text = resolve_5(path)
     print("Extracted Text", captcha_text)
