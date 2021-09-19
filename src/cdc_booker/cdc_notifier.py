d0a6049240e185ddea2d1d960a4324da6835f4d5
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2021-09-19 20:21:36 +0800

    add ability to send a photo through telegram

--- src/cdc_booker/cdc_notifier.py
+++ src/cdc_booker/cdc_notifier.py
@@ -2,13 +2,15 @@ import requests
 
 
 class CDCNotifier:
+    API_BASE_URL = "https://api.telegram.org/bot"
+
     def __init__(self, token, chat_id):
         self.token = token
         self.chat_id = chat_id
 
     def send_message(self, message):
         send_text = (
-            "https://api.telegram.org/bot"
+            CDCNotifier.API_BASE_URL
             + self.token
             + "/sendMessage?chat_id="
             + self.chat_id
@@ -17,4 +19,11 @@ class CDCNotifier:
         )
 
         response = requests.get(send_text)
+        return response.json()
+
+    def send_photo(self, image_path):
+        data = {"chat_id": self.chat_id}
+        url = f"{CDCNotifier.API_BASE_URL}{self.token}/sendPhoto"
+        with open(image_path, "rb") as image_file:
+            response = requests.post(url, data=data, files={"photo": image_file})
         return response.json()
